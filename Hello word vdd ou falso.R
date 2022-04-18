e750d6b8827837f3f5e723942d0905b91213fc25
Carlos henri Conceição Soares <79725152+CarlosHCS001@users.noreply.github.com>
2022-04-18 20:37:45 -0300

    Add files via upload

---
+++ Hello word vdd ou falso.R
@@ -0,0 +1,9 @@
+
+Hello.word <= 1
+Bye.word >= 2
+ 
+if (Hello.word== 1) {
+  cat(" Olá mundo é verdadeiro, Você já pode iniciar.")
+}  else if (Bye.word== 2) {
+  cat(" Huumm, Você está atrasado bora, bora correr atrás do prejuizo.")
+}
